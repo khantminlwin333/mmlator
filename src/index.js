448cb970589e57611fa7d3031c697dae3c770b62
khantminlwin <webdeveloperppro@gmail.com>
2025-03-24 16:31:27 +0630

    Remove Material-UI dependencies

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#6200ee',
-    },
-    secondary: {
-      main: '#03dac6',
-    },
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 ); 
